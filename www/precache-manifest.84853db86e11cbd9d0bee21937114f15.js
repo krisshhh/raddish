17c4c8740f28ee1f0722a1fb6fe687a58a168409
Krishna Vamshi <krishnavamshi.kairi2103@gmail.com>
2019-01-10 21:58:45 +0530

    init lorca

---
+++ www/precache-manifest.84853db86e11cbd9d0bee21937114f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5bedb331128b3fb04d72",
+    "url": "/static/js/main.5bedb331.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5bedb331128b3fb04d72",
+    "url": "/static/css/main.dff5c768.chunk.css"
+  },
+  {
+    "revision": "4cc44259c8b91f6367391e4e7e4a1c88",
+    "url": "/index.html"
+  }
+];
