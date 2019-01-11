504c28e29ef4f254f51367e8228d9c9e0438c799
Krishna Vamshi <krishnavamshi.kairi2103@gmail.com>
2019-01-12 02:37:59 +0530

    header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import { Container, Header, Icon } from 'semantic-ui-react'
+import { Container, Header,Breadcrumb, Icon, Grid, Search, Button, Image, Segment, Menu } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Container fluid>
-        <Header as='h2'>
-          <Icon name='settings' />
-          <Header.Content>
-            Radish
-            <Header.Subheader>Rabbitmq Client</Header.Subheader>
-          </Header.Content>
-        </Header>
-      </Container>
+    <Grid columns={1}>
+      <Grid.Row>
+        <Grid.Column>
+        {/* <Segment className="radish-header" clearing>
+          <Header floated='left' as='h2' image='/icon.png' content='Radish' />
+          <Button floated='right' icon='server' basic></Button>
+        </Segment> */}
+        <Menu fluid widths={3} borderless className="radish-nav">
+          <Menu.Item>
+            <Header  as='h2' className="radish-header" image='/icon.png' content='Radish' />
+          </Menu.Item>
+          <Menu.Item>
+          <Header as='h5'>
+            <Icon name='server' />
+            <Header.Content>Servers</Header.Content>
+          </Header>
+          </Menu.Item>
+          <Menu.Item>
+          <Button.Group
+            basic
+            buttons={[
+              { key: 'bold', icon: 'setting' },
+              { key: 'underline', icon: 'plug' },
+            ]}
+          />
+          </Menu.Item>
+        </Menu>
+        </Grid.Column>
+        {/* <Grid.Column>
+          <Search fluid />
+          <Button
+            primary
+            content='Add'
+            icon='add'
+          />
+          <Button
+            positive
+            content='Setting'
+            icon='setting'
+          />
+        </Grid.Column> */}
+      </Grid.Row>
+    </Grid>
     );
   }
 }
