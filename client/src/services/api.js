b02a2c81145b7a99fca1f1fe16dac95486b46120
Krishna Vamshi <krishnavamshi.kairi2103@gmail.com>
2019-01-16 01:23:34 +0530

    login binding

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,18 +1,30 @@
-import { of } from 'rxjs';
+import { of, throwError, Subject } from 'rxjs';
+import { catchError, mergeMap, map, take } from 'rxjs/operators';
 
 class Api {
 
     constructor() {
+        this.broker = new Subject();
+        window.onLogin = this.onLoginResponse;
+    }
+
+    onLoginResponse(response) {
+        const res =  JSON.parse(response);
+        this.broker.next(res);  
     }
 
     login(details) {
-        return of({
-            exchanges: [],
-            queues: [],
-            binds: []
-        });
+        window.Login(details);
+        const resp$ = this.broker.asObservable();
+        return resp$.pipe(take(1));
     }
 }
+
+// return throwError(new Error ({
+//     exchanges: [],
+//     queues: [],
+//     binds: []
+// }));
 
 const api = new Api();
 export default api;
--- main.go
+++ main.go
@@ -10,29 +10,22 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
-	"sync"
-
+	"encoding/json"
 	"github.com/zserge/lorca"
 )
 
 // Go types that are bound to the UI must be thread-safe, because each binding
 // is executed in its own goroutine. In this simple case we may use atomic
 // operations, but for more complex cases one should use proper synchronization.
-type counter struct {
-	sync.Mutex
-	count int
+type RabbitmqLoginDetails struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+	Username string `json:"userName"`
+	Password string `json:"password"`
 }
 
-func (c *counter) Add(n int) {
-	c.Lock()
-	defer c.Unlock()
-	c.count = c.count + n
-}
-
-func (c *counter) Value() int {
-	c.Lock()
-	defer c.Unlock()
-	return c.count
+type Rabbitmq struct {
+	Exchanges []string `json:"exchanges"`
 }
 
 func main() {
@@ -46,20 +39,45 @@ func main() {
 	}
 	defer ui.Close()
 
+	uilog := func (str string) {
+		ui.Eval(fmt.Sprintf("console.log('%s')", str));
+	}
+
+	parseLoginDetails := func (str string) RabbitmqLoginDetails {
+		var res RabbitmqLoginDetails
+		if err := json.Unmarshal([]byte(str), &res); err != nil {
+			fmt.Println(err);
+		}
+		return res 
+	}
+	
+	stringify := func (emp *Rabbitmq) string {
+		e, err := json.Marshal(emp)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return string(e);
+	}
+
 	// A simple way to know when UI is ready (uses body.onload event in JS)
 	ui.Bind("start", func() {
 		log.Println("UI is ready")
 	})
 
-	// Create and bind Go object to the UI
-	c := &counter{}
-	ui.Bind("counterAdd", c.Add)
-	ui.Bind("counterValue", c.Value)
+	ui.Bind("Login", func(str string) {
+		uilog(str);
+		det := parseLoginDetails(str);
+		fmt.Println(det)
+
+		// connect to rabbitmq
+
+		emp := &Rabbitmq{Exchanges: []string{"amp.fanout"}}
+		ui.Eval(fmt.Sprintf(`onLogin('%s')`, stringify(emp)))
+	})
 
 	// Load HTML.
 	// You may also use `data:text/html,<base64>` approach to load initial HTML,
 	// e.g: ui.Load("data:text/html," + url.PathEscape(html))
-
 	ln, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		log.Fatal(err)
@@ -67,13 +85,6 @@ func main() {
 	defer ln.Close()
 	go http.Serve(ln, http.FileServer(FS))
 	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
-
-	// You may use console.log to debug your JS code, it will be printed via
-	// log.Println(). Also exceptions are printed in a similar manner.
-	ui.Eval(`
-		console.log("Hello, world!");
-		console.log('Multiple values:', [1, false, {"x":5}]);
-	`)
 
 	// Wait until the interrupt signal arrives or browser window is closed
 	sigc := make(chan os.Signal)
